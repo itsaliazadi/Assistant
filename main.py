9e53346ad1ff2a6501e82737ce54be6a8db57542
Ali Azadi <aliazadicode@gmail.com>
2024-07-16 19:03:04 +0330

    Now the set_alarm_clock is called when needed using multithreading

---
+++ main.py
@@ -0,0 +1,41 @@
+import speech_recognition as sr
+import Reminder
+import threading
+
+# Create a Recognizer instance
+recognizer = sr.Recognizer()
+
+reminder_words = ["remind", "alarm", "alert", "notify"]
+
+while True:
+    # Capture audio input from the microphone
+    with sr.Microphone() as source:
+        print("Speak something...")
+        audio_data = recognizer.listen(source, phrase_time_limit=10)
+
+    # Perform speech recognition using Google Web Speech API
+    try:
+        text = recognizer.recognize_google(audio_data)
+        print(f"You said:{text}")
+
+        # Reminder
+        for word in reminder_words:
+            if word in text:
+                # Remind in
+                if "in" in text:
+                    print("in...")
+                # Alarm clock
+                else:
+                    # Reminder.set_alarm_clock(text)
+                    threading.Thread(target=Reminder.set_alarm_clock, args=(text,)).start()
+                break
+
+
+        if (text == "Done" or text == "done"):
+            break
+
+
+    except sr.UnknownValueError:
+        print("Sorry, could not understand audio.")
+    except sr.RequestError as e:
+        print("Error: Could not request results from Google Speech Recognition service;")
