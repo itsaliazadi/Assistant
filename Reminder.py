9622ca353bc88e01985d1f2a20649d9dbf09914c
Ali Azadi <aliazadicode@gmail.com>
2024-07-16 19:04:42 +0330

    Changed the prototype of the set_alarm_clock function.Now it takes the text and extracts the time.

--- Reminder.py
+++ Reminder.py
@@ -1,18 +1,38 @@
 # Simple Virtual Assistant: Create a simple virtual assistant that can perform tasks like setting reminders, sending emails, or making phone calls. You could use Python's speech recognition and text-to-speech libraries for this project.
 
-
+import re
 import time
-import datetime
+from datetime import datetime
 import pygame
 
 
 
 
-def set_alarm_clock(time_to_remind, task):
+def set_alarm_clock(text):
+
+    time_to_remind = ""
+
+    # Extracting the time
+    pattern = r"for (\d{1,2}:\d{2})"
+    match = re.search(pattern, text)
+    time_string = match.group(1)
+    
+    # Converting the time into the standard form
+    if "p.m" in text:
+        time_string += " pm"
+        dt = datetime.strptime(time_string, "%I:%M %p")
+        standard_time_string = dt.strftime("%H:%M")
+        time_to_remind = standard_time_string
+
+    else:
+        time_to_remind = time_string
+
+
+
     pygame.init()
     pygame.mixer.init()
     while True:
-        current_time = datetime.datetime.now().strftime('%H:%M')
+        current_time = datetime.now().strftime('%H:%M')
         if current_time == time_to_remind:
             pygame.mixer.music.load("C:\\Users\\User\\Desktop\\Assistant_project\\Assistant\\Sound.mp3")
             pygame.mixer.music.play()
