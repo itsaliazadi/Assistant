34b4d22d3f66fb0ae2244b32e172ddfc5c6d3105
Ali Azadi <aliazadicode@gmail.com>
2024-07-17 13:56:57 +0330

    Wrote the remind_in function

--- Reminder.py
+++ Reminder.py
@@ -2,8 +2,8 @@
 
 import re
 import time
-from datetime import datetime
 import pygame
+from datetime import datetime, timedelta
 
 
 
@@ -42,4 +42,45 @@ def set_alarm_clock(text):
         time.sleep(1)
 
 
+def remind_in(text):
+
+    hour = 0
+    minute = 0
+    second = 0
+
+    # Extracting the hours and minutes and seconds
+    hour_pattern = r"(\d+) hour(?:s)?"
+    minute_pattern = r"(\d+) minute(?:s)?"
+    second_pattern = r"(\d+) second(?:s)?"
+
+    hour_match = re.search(hour_pattern, text)
+    minute_match = re.search(minute_pattern, text)
+    second_match = re.search(second_pattern, text)
+
+    if hour_match:
+        hour = int(hour_match.group(1))
+    if minute_match:
+        minute = int(minute_match.group(1))
+    if second_match:
+        second = int(second_match.group(1))
+
+    
+    future_time = datetime.now() + timedelta(hours=hour, minutes=minute, seconds=second)
+    time_to_remind = future_time.strftime('%H:%M:%S')
+
+    pygame.init()
+    pygame.mixer.init()
+
+    while True:
+        current_time = datetime.now().strftime('%H:%M:%S')
+        if (current_time == time_to_remind):
+            pygame.mixer.music.load("C:\\Users\\User\\Desktop\\Assistant_project\\Assistant\\Sound.mp3")
+            pygame.mixer.music.play()
+            while pygame.mixer.music.get_busy():
+                time.sleep(0.1)
+            break
+        time.sleep(1)
+
+
+    
 
