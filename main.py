9639d1d60dc797353f04bb65fab22803574a03a3
Ali Azadi <aliazadicode@gmail.com>
2024-07-19 18:04:45 +0330

    Added some statements to handle the point where the user asks about the weather

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
 import speech_recognition as sr
+import Weather
+import re
+import spacy
 
 # Create a Recognizer instance
 recognizer = sr.Recognizer()
 
 reminder_words = ["remind", "alarm", "alert", "notify"]
+weather_words = ["weather", "climate"]
 
 while True:
     # Capture audio input from the microphone
     with sr.Microphone() as source:
         print("Speak something...")
-        audio_data = recognizer.listen(source, phrase_time_limit=10)
+        audio_data = recognizer.listen(source, phrase_time_limit=5)
 
     # Perform speech recognition using Google Web Speech API
     try:
@@ -25,6 +29,18 @@ while True:
                 else:
                     print(text)
                 break
+
+        # Weather
+        for word in weather_words:
+            if word in text:
+                print(Weather.describeWeather(text))
+                break
+
+        # Temperature
+        if "temperature" in text:
+            print(Weather.getTemperature(text))
+    
+
 
 
         if (text == "Done" or text == "done"):
