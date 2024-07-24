8c485cab91161f315b9534b53f39bce9d04f00eb
Ali Azadi <aliazadicode@gmail.com>
2024-07-24 14:27:37 +0330

    Made a function to get a summary of the desired word after the word is extracted from the user speech

---
+++ Wikipedia.py
@@ -0,0 +1,25 @@
+import wikipedia
+
+
+def getASummaryOf(text):
+
+    word = extractWord(text)
+    
+    summary = wikipedia.summary(word, sentences=5)
+    
+    return summary
+
+
+
+def extractWord(text):
+
+    result = text.split("of", 1)[1].strip()
+    result = result.replace(" ", "")
+
+    return result
+
+
+print(getASummaryOf("Give me a summary of linear regression"))
+
+
+
